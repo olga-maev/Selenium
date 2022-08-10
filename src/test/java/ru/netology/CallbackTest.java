6adb44fbdc596f84fe8f486d6ccb27ddf1006da3
olga-maev <kurumoch0@gmail.com>
2022-08-10 21:33:19 +0400

    Finish commit

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -41,7 +41,7 @@ class CallbackTest {
         driver = null;
     }
     @Test
-    void shouldTest1() throws InterruptedException {
+    void shouldTestPositive1() throws InterruptedException {
         driver.get("http://localhost:9999/");
         List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Иванов Иван");
@@ -51,6 +51,90 @@ class CallbackTest {
 
         String text = driver.findElement(By.className("heading_theme_alfa-on-white")).getText();
         assertEquals("Заявка на дебетовую карту", text.trim());
+    }
+    @Test
+    void shouldTestPositive2() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванов-Петров Иван");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("heading_theme_alfa-on-white")).getText();
+        assertEquals("Заявка на дебетовую карту", text.trim());
+    }
+    @Test
+    void shouldTestPositive3() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванов Иван-Сергей");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("heading_theme_alfa-on-white")).getText();
+        assertEquals("Заявка на дебетовую карту", text.trim());
+    }
+    @Test
+    void shouldTestPositive4() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Д'Иванов Иван");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("heading_theme_alfa-on-white")).getText();
+        assertEquals("Заявка на дебетовую карту", text.trim());
+    }
+    @Test
+    void shouldTestNegative1() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Ivanov Иван");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+    @Test
+    void shouldTestNegative2() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванов Ivan");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+    @Test
+    void shouldTestNegative3() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("1233 Иван");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+    }
+    @Test
+    void shouldTestNegative4() throws InterruptedException {
+        driver.get("http://localhost:9999/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иванов 123");
+        elements.get(1).sendKeys("+79874561221");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 
 }
